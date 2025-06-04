c0b7b83793ea17533524ecb1266a3f922f4cfde7
adamyu <adamyxt@outlook.com>
2025-06-04 16:02:50 +0800

    自定义文本框以及password属性依赖实现

--- WPF_LoginUI/LoginVM.cs
+++ WPF_LoginUI/LoginVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using System.Windows.Input;
 
 namespace WPF_LoginUI
 {
-    public class LoginVM : INotifyPropertyChanged
+    public class LoginVM : DependencyObject, INotifyPropertyChanged
     {
         private MainWindow _main;
         public LoginVM(MainWindow main)
@@ -34,19 +35,21 @@ namespace WPF_LoginUI
             }
         }
 
-        public string Password
-        {
-            get { return _loginModel.Password; }
-            set
-            {
-                _loginModel.Password = value;
-                OnPropertyChanged("Password");
-            }
-        }
+        //public string Password
+        //{
+        //    get { return _loginModel.Password; }
+        //    set
+        //    {
+        //        _loginModel.Password = value;
+        //        OnPropertyChanged("Password");
+        //    }
+        //}
 
         void LoginFunc()
         {
-            if (Username == "wpf" && Password == "666")
+            Console.WriteLine(_loginModel.Username);
+            Console.WriteLine(_loginModel.Password);
+            if (Username == "wpf" && MyPasswordVM == "456")
             {
                 //MessageBox.Show("ok");
                 Index index = new Index();
@@ -58,8 +61,9 @@ namespace WPF_LoginUI
             {
                 MessageBox.Show("输入的用户名或者密码不正确");
                 Username = "";
-                Password = "";
+                MyPasswordVM = "";
             }
+            //MyPasswordVM = DateTime.Now.ToString("HH:MM:ss.ffff");
         }
 
         bool CanLoginExecute()
@@ -74,5 +78,19 @@ namespace WPF_LoginUI
                 return new RelayCommond(LoginFunc, CanLoginExecute);
             }
         }
+
+
+        public string MyPasswordVM
+        {
+            get { return (string)GetValue(MyPasswordVMProperty); }
+            set { SetValue(MyPasswordVMProperty, value); }
+
+        }
+
+        // Using a DependencyProperty as the backing store for MyPasswordVM.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPasswordVMProperty =
+            DependencyProperty.Register("MyPasswordVM", typeof(string), typeof(LoginVM));
+
+
     }
 }
---
+++ WPF_LoginUI/PasswordBoxHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_LoginUI
+{
+    public class PasswordBoxHelper
+    {
+        public static string GetPwd(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PwdProperty);
+        }
+
+        public static void SetPwd(DependencyObject obj, string value)
+        {
+            obj.SetValue(PwdProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Pwd.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PwdProperty =
+            DependencyProperty.RegisterAttached("Pwd", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata(string
+                .Empty, OnPwdPropertyChanged));
+
+        private static void OnPwdPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox pwdbox = d as PasswordBox;
+            if (pwdbox == null)
+                return;
+            pwdbox.Password = (string)e.NewValue;
+            SetSelection(pwdbox, pwdbox.Password.Length, 0);
+        }
+
+        private static void SetSelection(PasswordBox passwordBox, int start, int length)
+        {
+            passwordBox.GetType()
+               .GetMethod("Select", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+               .Invoke(passwordBox, new object[] { start, length });
+        }
+
+        public static bool GetIsEnablePasswordPropertyChanged(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsEnablePasswordPropertyChangedProperty);
+        }
+
+        public static void SetIsEnablePasswordPropertyChanged(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnablePasswordPropertyChangedProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsEnablePasswordPropertyChanged.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsEnablePasswordPropertyChangedProperty =
+            DependencyProperty.RegisterAttached("IsEnablePasswordPropertyChanged", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnPasswordPropertyChanged));
+
+        private static void OnPasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox pwdbox = d as PasswordBox;
+            if (pwdbox == null)
+                return;
+            if ((bool)e.NewValue)
+            {
+                pwdbox.PasswordChanged += MyPsswordPropertyChanged;
+            }
+            else
+            {
+                pwdbox.PasswordChanged -= MyPsswordPropertyChanged;
+            }
+        }
+
+        private static void MyPsswordPropertyChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox box = (PasswordBox)sender;
+            SetPwd(box, box.Password);
+        }
+    }
+}
